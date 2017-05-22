8eca2816ea092e83f9a54044a0266ec3f7700d63
Peter Tan <pxhtan@gmail.com>
2017-05-22 14:34:53 -0700

    Working beesbeesbees

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.honeyPot = 0;
+  this.age = 10;
+  this.job = 'make honey';
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+// HoneyMakerBee.age = 10;
+// HoneyMakerBee.job = 'make honey';
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
